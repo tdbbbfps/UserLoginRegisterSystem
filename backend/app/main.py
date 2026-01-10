dd6926fa69d747b513fa379fc7cf29480feabb98
tdbbb <tdbbb0427@gmail.com>
2026-01-10 20:20:17 +0800

    Feat: Add a get endpoint.

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,6 +23,9 @@ app = FastAPI(
 
 app.include_router(user_router, prefix="/api/users", tags=["users"])
 
+@app.get('/')
+async def root():
+    return {"message" : "Here's root of the API."}
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
