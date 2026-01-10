d8049f3816c2f5ffe01e45afeb72ac2fb7666cdf
tdbbb <tdbbb0427@gmail.com>
2026-01-10 20:21:30 +0800

    Feat: Add create user endpoint.

--- backend/app/api/user.py
+++ backend/app/api/user.py
@@ -1,8 +1,22 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from database.database import get_database
+from models import user as user_model
+from schemas import user as user_schema
+from crud import user as user_crud
 
 router = APIRouter()
 
 @router.get('/')
 async def root() -> dict:
     return {"message" : "Here's User API endpoint."}
+
+@router.post('/create', response_model=user_schema.User, status_code=status.HTTP_201_CREATED)
+async def create_user(user : user_schema.UserCreate, db : Session = Depends(get_database)):
+    try:
+        db_user = user_crud.create_user(db, user)
+        if not db_user:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Failed creating user.")
+        return db_user
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
