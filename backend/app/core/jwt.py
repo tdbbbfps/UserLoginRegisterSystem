8606a035d39ecdc2159b7b83a766928972f57de3
tdbbb <tdbbb0427@gmail.com>
2026-01-11 22:23:35 +0800

    Feat: Create and verify access token.

---
+++ backend/app/core/jwt.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+from typing import Optional
+from jose import jwt, JWTError
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+def create_access_token(data : dict, expires_delta : Optional[timedelta] = None):
+    data_to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    # Add expiration time to the payload (exp is a standard claim name for expiration time.)
+    data_to_encode.update({"exp" : expire})
+    
+    encode_jwt = jwt.encode(data_to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encode_jwt
+
+def verify_access_token(token : str) -> Optional[dict]:
+    """Verify the access token and return the payload if valid."""
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
