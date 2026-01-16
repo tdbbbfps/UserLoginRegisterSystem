d071b00548db2cd957aaf44c3e726f471460b440
tdbbb <tdbbb0427@gmail.com>
2026-01-16 20:17:29 +0800

    Fix: Remove password field in UserUpdate due to a new schema PasswordUpdate.

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -4,7 +4,7 @@ from typing import Optional
 # Base schema for User. Includes common and public fields(email, username, bio).
 class UserBase(BaseModel):
     email : EmailStr = Field(..., description="The email address of the user.")
-    username : str = Field(..., min_length=3, max_length=50, description="The username of the user.")
+    username : str = Field(..., min_length=1, max_length=100, description="The username of the user.")
     name : str = Field(..., min_length=1, max_length=100, description="The display name of the user.")
     bio : Optional[str] = Field(None, description="A brief self-introduction of the user.")
 
@@ -16,7 +16,6 @@ class UserCreate(UserBase):
 class UserUpdate(BaseModel):
     email : Optional[EmailStr] = Field(None, description="The new email address of the user.")
     username : Optional[str] = Field(None, min_length=3, max_length=50, description="The new username of the user.")
-    password : Optional[str] = Field(None, min_length=8, description="The new password for the user account.")
     name : Optional[str] = Field(None, min_length=1, max_length=100, description="The display name of the user.")
     bio : Optional[str] = Field(..., description="A brief self-introduction of the user.")
 
